ebfd36a9010e35f901aac2090da796720d2da0a2
spande2 <spande2@hawk.iit.edu>
2019-03-06 14:30:06 -0600

    333

--- cpu.c
+++ cpu.c
@@ -60,7 +60,7 @@ void * fpoperations(void * arg)
 		__m256 evens = _mm256_set_ps(2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0);
 		__m256 odds = _mm256_set_ps(1.0, 3.0, 5.0, 7.0, 9.0, 11.0, 13.0, 15.0);
 		
-		long double no_of_operations=1000000000000;
+		long double no_of_operations=100000000000;
         int outerloop = (no_of_operations)/1000000;
         int result1= (outerloop)/no_threads;
 		
