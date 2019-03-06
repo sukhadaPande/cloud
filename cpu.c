b37407ac023e16bfe6f2638322373a83e621cf04
spande2 <spande2@hawk.iit.edu>
2019-03-06 00:08:23 -0600

    avx

--- cpu.c
+++ cpu.c
@@ -53,7 +53,7 @@ void perform_FLOPS(int no_threads)
 
 void * fpoperations(void * arg)
 {
-		
+		int no_threads = (int)(intptr_t)arg;
         __m256 evens = _mm256_set_ps(2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0,18.0,20.0);
 		__m256 odds = _mm256_set_ps(1.0, 3.0, 5.0, 7.0, 9.0, 11.0, 13.0, 15.0,17.0,19.0);
 		
