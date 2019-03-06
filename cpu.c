aa48e8321a7c488ff8ef24655572e588f9452de5
spande2 <spande2@hawk.iit.edu>
2019-03-06 01:01:35 -0600

    22

--- cpu.c
+++ cpu.c
@@ -5,7 +5,10 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <sys/time.h>
+
+
 #include <immintrin.h>
+#include <stdio.h>
 
 
 const int no_loops = 1000000;
@@ -54,20 +57,22 @@ void perform_FLOPS(int no_threads)
 void * fpoperations(void * arg)
 {
 		int no_threads = (int)(intptr_t)arg;
-        __m256 evens = _mm256_set_ps(2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0,18.0,20.0);
-		__m256 odds = _mm256_set_ps(1.0, 3.0, 5.0, 7.0, 9.0, 11.0, 13.0, 15.0,17.0,19.0);
+		__m256 evens = _mm256_set_ps(2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0);
+		__m256 odds = _mm256_set_ps(1.0, 3.0, 5.0, 7.0, 9.0, 11.0, 13.0, 15.0);
 		
-		long double no_of_operations=100000000000;
+		long double no_of_operations=1000000000000;
         int outerloop = (no_of_operations)/1000000;
         int result1= (outerloop)/no_threads;
-		_m256 result = _mm256_sub_ps(evens, odds);
+		
 
 		for (int z = 0; z < (1000000) ; z++)
 		 {
                 for(int y =0; y <(result1);y++)
                 {
+					
+				__m256 result = _mm256_sub_ps(evens, odds);
                 float* f = (float*)&result;
-				printf("%f %f %f %f %f %f %f %f\n",f[0], f[1], f[2], f[3], f[4], f[5], f[6], f[7]);
+				
                 }
         }
 		
